22ee0ceb8ef1eeeeffaf777a86f977dce3d6d1c6
lollobianco <loreb2002@gmail.com>
2022-11-24 15:39:20 +0100

    Create card dinamicamente

--- src/components/main/main_cards/DcCard.vue
+++ src/components/main/main_cards/DcCard.vue
@@ -1,15 +1,52 @@
 <template>
-   <div>
+   <div class="card">
+      
+      <div class="img-container">
+         <img :src="productDetails.thumb" alt="ProductImage">
+      </div>
+      
 
+      <div class="detail">{{productDetails.series.toUpperCase()}}</div>
+      
    </div>
 </template>
 
 <script>
    export default {
-      name: 'DcCard'
+      name: 'DcCard',
+      props:{
+         productDetails: Object
+      }
    }
 </script>
 
 <style lang="scss" scoped>
+
+.card{
+   width: calc(100%/6);
+   padding: 20px 0;
+   text-align: left;
+   color: white;
+   font-weight: 700;
+   font-size: 0.8rem;
+}
+
+.img-container{
+   width: 85%;
+   margin: 0 auto;
+
+   img{
+      width: 100%;
+      aspect-ratio: 1/1;
+      object-fit:cover;
+      object-position: top;
+   }
+
+}
+
+.detail{
+   width: 85%;
+   margin: 0 auto;
+}
 
 </style>
